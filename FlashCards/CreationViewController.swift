a6b672782842667ac89239fcfad5d9453f8e58e8
Peter Chuong <peter@peters-macbook-pro.local>
2018-10-27 13:54:17 -0700

    Completed the first two requirements

---
+++ FlashCards/CreationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreationViewController.swift
+//  FlashCards
+//
+//  Created by Peter Chuong on 10/27/18.
+//  Copyright © 2018 Peter Chuong. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
